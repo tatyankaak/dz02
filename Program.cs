ed40dc5e49bb21f6e551920bb2a2c0bab14d9d3c
Татьяна <robsten9086@mail.ru>
2023-07-09 15:33:25 +0300

    задача 1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое число:");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число:");
+        int b = int.Parse(Console.ReadLine());
+
+        int max = Math.Max(a, b);
+        int min = Math.Min(a, b);
+
+        Console.WriteLine($"Максимальное число: {max}");
+        Console.WriteLine($"Минимальное число: {min}");
+
+        Console.ReadKey();
+    }
+}
